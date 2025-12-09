49f9f4b7ce8a8c580cbeeb7bd037e736a85ce516
Seungwon Lee <seungwon.dev@gmail.com>
2025-12-09 15:02:47 +0900

    fix: 푸시 구독관련 로직 실행시 중복 요청이 가능하던 문제 수정

--- src/hooks/usePushSubscription.js
+++ src/hooks/usePushSubscription.js
@@ -5,24 +5,37 @@ import urlBase64ToUint8Array from "../utils/urlBase64ToUint8Array";
 import useApi from "./useApi";
 
 const usePushSubscription = () => {
-  const { execute: subscribe, loading: subscribeLoading } = useApi(pushService.subscribe);
-  const { execute: unsubscribe, loading: unsubscribeLoading } = useApi(pushService.unsubscribe);
+  const { execute: subscribe } = useApi(pushService.subscribe);
+  const { execute: unsubscribe } = useApi(pushService.unsubscribe);
+  const [subscribeLoading, setSubscribeLoading] = useState(false);
+  const [unsubscribeLoading, setUnsubscribeLoading] = useState(false);
   const [isSubscribed, setIsSubscribed] = useState(false);
 
   const doSubscribe = async () => {
-    const reg = await navigator.serviceWorker.ready;
-    const sub = await reg.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(import.meta.env.VITE_VAPID_KEY)
-    });
-
+    setSubscribeLoading(true);
     try {
-      await subscribe(sub);
-      setIsSubscribed(true);
+      const reg = await navigator.serviceWorker.ready;
+      const sub = await reg.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(import.meta.env.VITE_VAPID_KEY)
+      });
+
+      try {
+        await subscribe(sub);
+        setIsSubscribed(true);
+      } catch (err) {
+        await sub.unsubscribe();
+        setIsSubscribed(false);
+        throw err;
+      }
     } catch (err) {
-      await sub.unsubscribe();
-      setIsSubscribed(false);
+      const permission = Notification.permission;
+      if (permission === "denied") {
+        throw Error("알림 권한을 허용해주세요.");
+      }
       throw err;
+    } finally {
+      setSubscribeLoading(false);
     }
   };
 
@@ -36,10 +49,12 @@ const usePushSubscription = () => {
     }
 
     try {
+      setUnsubscribeLoading(true);
       await unsubscribe(sub.endpoint);
     } finally {
       await sub.unsubscribe();
       setIsSubscribed(false);
+      setUnsubscribeLoading(false);
     }
   };
 
